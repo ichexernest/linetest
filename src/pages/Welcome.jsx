aee1dba252736b544e9dae10ad75e2877a58466f
Ernest Chen <ch82831223@gmail.com>
2024-07-27 16:55:45 +0800

    UI

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Title from '/images/title.png';
+import Logo from '/images/logo.png';
 import { useRoute } from '../provider/routeProvider';
 
 const Welcome = () => {
@@ -29,20 +30,37 @@ const Welcome = () => {
 
     if (!isMobile) {
         return (
-            <div className="flex items-center justify-center h-screen">
-                <div className="text-center">
-                    <p className="text-2xl mb-4">請使用手機進行訪問此頁面</p>
+            <div className="flex flex-col items-center justify-between h-screen">
+                      <div className='w-screen flex justify-center items-center mt-10'>
+        <img src={Title} className='w-2/5' alt="Title" />
+      </div>
+                <div className="text-center mb-44">
+                    <p className="text-2xl mb-4 font-bold text-blue-900">請使用手機進行訪問此頁面</p>
                 </div>
+                <div className='w-screen flex justify-center items-center mb-10'>
+        <img src={Logo} className='w-[250px]' alt="Logo" />
+      </div>
             </div>
         );
     }
 
     return (
         <>
-            <img src={Title} className='w-4/5' alt="Title" />
-            <p>this is a welcome page, can display some activity rule, policy, or something</p>
-                <button className='p-5 text-2xl bg-amber-400 text-white' onClick={handleTurnPage}> start </button>
+      <div className='w-screen flex justify-center items-center mt-5'>
+        <img src={Title} className='w-4/5' alt="Title" />
+      </div>
+      <div className='bg-white bg-opacity-40 border-blue-900 border-4 m-6 rounded-lg p-6'>
+      <p className='text-blue-900 font-bold text-lg'>每位爸爸都是家庭中的無名英雄，默默付出，守護著我們的幸福與健康。無論是清晨的咖啡，還是深夜的加班，他們總是在我們身邊，給予無限的支持和愛。</p>
+
+      <p className='text-blue-900 font-bold text-lg mt-3'>這個父親節，讓我們回饋這份愛。GoodMood 誠摯邀請您為爸爸設計專屬卡片，滿載感謝與愛意。分享卡片後，您還可獲得特別優惠券，享受更多優惠。</p>
+
+      <p className='text-blue-900 font-bold text-lg mt-3'>立即開始設計，為最愛的超級英雄送上滿滿的力量和愛吧！</p>
+      </div>
+                <button className='p-5 text-2xl bg-[#FED501] text-blue-900 rounded-lg shadow-md font-extrabold border-4 border-blue-800' onClick={handleTurnPage}> 馬上開始 </button>
             {/* <Link to={`/share`}> <button> share </button></Link> */}
+            <div className='w-screen flex justify-center items-center mb-10'>
+        <img src={Logo} className='w-[200px]' alt="Logo" />
+      </div>
         </>
     );
 }
